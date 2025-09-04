8b8af8c4dd2ab4be5ae6f4287f3dbecd2c4ad46b
Reevun <jinsasinmu0627@gmail.com>
2025-09-04 14:34:39 +0700

    sql crm fix

--- crm_app.sql
+++ crm_app.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Aug 28, 2025 at 10:27 AM
+-- Generation Time: Sep 04, 2025 at 07:33 AM
 -- Server version: 8.0.30
 -- PHP Version: 8.2.29
 
@@ -31,15 +31,16 @@ CREATE TABLE `crm` (
   `company_email` varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `marketing_id` char(3) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `marketing_person_name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `contact_person_email` varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `person_email` varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `company_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `contact_person` varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `name_person` varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `contact_person_position_title` varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `phone_wa` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `phone_number` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `phone_number2` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `company_website` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `company_category` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `contact_person_position_category` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `company_industry_type` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `company_type` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `address` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
   `city` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `postcode` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
@@ -52,8 +53,9 @@ CREATE TABLE `crm` (
 -- Dumping data for table `crm`
 --
 
-INSERT INTO `crm` (`company_email`, `marketing_id`, `marketing_person_name`, `contact_person_email`, `company_name`, `contact_person`, `contact_person_position_title`, `phone_wa`, `company_website`, `company_category`, `contact_person_position_category`, `company_industry_type`, `address`, `city`, `postcode`, `status`, `created_at`, `updated_at`) VALUES
-('Antara@gmail.com', 'FRL', 'Farelfadlillah', 'Example@gmail.com', 'PT. Anatara', 'Bpk ...', 'IT SUPPORT', '0811...', 'antara.com', 'Public', 'IT', 'insurence', 'Jl...', 'Jakarta', '10410', 'emailed', '2025-08-27 05:16:26', '2025-08-27 05:16:39');
+INSERT INTO `crm` (`company_email`, `marketing_id`, `marketing_person_name`, `person_email`, `company_name`, `name_person`, `contact_person_position_title`, `phone_number`, `phone_number2`, `company_website`, `company_category`, `contact_person_position_category`, `company_type`, `address`, `city`, `postcode`, `status`, `created_at`, `updated_at`) VALUES
+('Antara@gmail.com', 'FRL', 'Farelfadlillah', 'Example@gmail.com', 'PT. Anatara', 'Bpk ...', 'IT SUPPORT', '0811...', '', 'antara.com', 'Public', 'IT', 'insurence', 'Jl...', 'Jakarta', '10410', 'emailed', '2025-08-27 05:16:26', '2025-08-27 05:16:39'),
+('rayterton@gmail.con', 'BTG', NULL, 'tes@gmail.com', 'PT Repliaa', 'Kangkung', 'PM', '0887741241', '', 'digidaw', 'iya', 'iyadeh', 'Mobil', 'JL iya', 'Jakarta', '103020', 'input', '2025-09-04 06:00:22', '2025-09-04 07:25:25');
 
 -- --------------------------------------------------------
 
@@ -115,7 +117,8 @@ INSERT INTO `users` (`marketing_id`, `name`, `email`, `password`, `created_at`) 
 --
 ALTER TABLE `crm`
   ADD PRIMARY KEY (`company_email`),
-  ADD KEY `marketing_id` (`marketing_id`);
+  ADD KEY `marketing_id` (`marketing_id`),
+  ADD KEY `company_email` (`company_email`);
 
 --
 -- Indexes for table `users`
